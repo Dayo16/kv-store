57edb4283fe0f1c107559a95baea2e6745f0de90
Dayo Oladapo <masterdayo1@hotmail.com>
2026-05-23 22:52:10 +0200

    Implemented kv_put, to add entries

--- src/main.c
+++ src/main.c
@@ -2,10 +2,18 @@
 #include "kv.h"
 
 int main() {
-    kv_t *table = kv_init(3);
-    printf("%p \n", table);
+    kv_t *table = kv_init(1024);
+    printf("Table pointer: %p \n", table);
     printf("Capacity: %zu\n", table->capacity);
-    printf("Capacity: %ld\n", table->capacity);
 
+    kv_put (table, "key1", "valueAltered");
+    kv_put (table, "key2", "value2");
+    kv_put (table, "key3", "value2");
+
+    for (int i = 0; i < table->capacity; i++) {
+        kv_entry_t entry = table->entries[i];
+        if (!entry.key) continue; // Skip empty entries
+        printf("Entry [%d]: key=%s, value=%s\n", i, entry.key, entry.value);
+    }
     return 0;
 }
